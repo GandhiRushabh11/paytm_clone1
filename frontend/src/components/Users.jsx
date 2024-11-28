bafa4622f7b8b2ee3fe229e029c46b96f0c4b7ea
Rushabh <gandhirushabh11@gmail.com>
2024-11-28 23:40:13 +0530

    Added Filter functionality

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -4,20 +4,21 @@ import { UserGrid } from "./UserGrid";
 
 export const Users = () => {
   const [users, setUsers] = useState([]);
+  //Need to apply debouncing here 
+  const [filter, setFilter] = useState("");
   useEffect(() => {
     const userToken = localStorage.getItem("token");
     async function getAllUsers() {
       const response = await axios.get(
-        "http://localhost:5000/api/v1/user/bulk",
+        "http://localhost:5000/api/v1/user/bulk"+`?filter=${filter}`,
         {
           headers: { Authorization: `Bearer ${userToken}` },
         }
       );
-      console.log(response.data.data);
       setUsers(response.data.data);
     }
     getAllUsers();
-  }, []);
+  }, [users]);
 
   return (
     <>
@@ -25,6 +26,9 @@ export const Users = () => {
       <div className="mt-4 mb-9">
         <input
           type="text"
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
           placeholder="Search users..."
           className="w-full rounded px-2 py-1 border  border-slate-200"
         ></input>
